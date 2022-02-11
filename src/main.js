6ea2b54ab9dea46a65210a0c8a35c22aa9314b18
William R <wiribasczky@gmail.com>
2022-02-11 17:12:06 -0300

    Remoção de plugin desnecessario

--- src/main.js
+++ src/main.js
@@ -20,26 +20,6 @@ const vMaskV3 = {
     unmounted: vMaskV2.unbind
 };
 
-const laravelErrors = {
-    install(Vue, options) {
-        Vue.laravelError = (e, message) => {
-            let response = e.response;
-            if (response && response.status === 422 && response.data.errors) {
-                let erro = Object.keys(response.data.errors)[0];
-                this.$toast.open({
-                    message: response.data.errors[erro][0],
-                    type: 'error'
-                });
-            } else {
-                this.$toast.open({
-                    message: message,
-                    type: 'error'
-                });
-            }
-        }
-    },
-}
-
 let LaravelError = {
     install: (app, options) => {
         // inject a globally available $translate() method
